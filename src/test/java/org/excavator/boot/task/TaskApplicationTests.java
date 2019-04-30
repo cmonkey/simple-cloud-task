dda4674a841edf6e0b51bf6cddfd0265ae5762e8
CMonkey <42.codemonkey@gmail.com>
2019-04-30 11:16:05 +0800

    rebase

--- src/test/java/org/excavator/boot/task/TaskApplicationTests.java
+++ src/test/java/org/excavator/boot/task/TaskApplicationTests.java
@@ -10,6 +10,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskApplicationTests {
