d54451ef720bf1ca44ff2664a2e37121bb09b8e9
CMonkey <42.codemonkey@gmail.com>
2019-04-30 11:34:20 +0800

    add EnableTask to PlanetJonesHelloTask

--- src/main/java/org/excavator/boot/task/PlanetJonesHelloTask.java
+++ src/main/java/org/excavator/boot/task/PlanetJonesHelloTask.java
@@ -3,11 +3,13 @@ package org.excavator.boot.task;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.IntStream;
 
 @SpringBootApplication
+@EnableTask
 public class PlanetJonesHelloTask {
 
     public static void main(String[] args) {
